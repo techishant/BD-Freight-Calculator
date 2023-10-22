58033329bb78c5e94672239fc03e896133a57701
Ishant Sharma <ishantsh12234@gmail.com>
2023-10-22 17:31:31 +0530

    Updated theme selector

--- designScript.js
+++ designScript.js
@@ -1,17 +1,21 @@
 const themeSelector = document.getElementById("themeSelector");
 
 
-let acc_col_arr = ["#6a1b9a", "#c62828", "#283593", "#2e7d32", "#00796b", "#0097a7", "#ad1457"]
+let acc_col_arr = ["#6a1b9a", "#c62828", "#283593", "#2e7d32", "#00796b", "#0097a7", "#ad1457", -1]
 document.body.style.setProperty('--acc-color', localStorage.getItem("acc-color"));                                           
 
 let tempDiv;
 
 for(i = 0; i<acc_col_arr.length; i++){
     tempDiv = document.createElement("span");
-    tempDiv.style.background = `${acc_col_arr[i]}`
-    tempDiv.addEventListener("click", function(){
+    if(acc_col_arr[i] == -1){
+        tempDiv.classList.add("thm-icon");
+    }
+    else{
+        tempDiv.style.background = `${acc_col_arr[i]}`;
+        tempDiv.addEventListener("click", function(){
         document.body.style.setProperty('--acc-color', this.style.background);
         localStorage.setItem("acc-color", this.style.background);
-    })
+    })}
     themeSelector.appendChild(tempDiv);
 }
